48650c841ba13b517b312384ec069b0c32d628fa
michalziaja <135656401+michalziaja@users.noreply.github.com>
2023-07-24 15:09:51 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
-# Google Cloud Platform
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.74.0"
+    }
+  }
+}
 provider "google" {
     credentials = file(key.json)
     project = "rational-autumn-393513"
